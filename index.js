a0722649bfbf1e859bdf0bcc98cd91b1f7b4705e
hasanmehedi9034 <mehedi90344@gmail.com>
2023-01-21 11:44:32 +0600

    node module system

---
+++ index.js
@@ -0,0 +1,8 @@
+const {people, a, test} = require('./people')
+// const people = require('./people')
+const _ = require('loadsh')
+
+console.log(_.last(people))
+
+
+
---
+++ people.js
@@ -0,0 +1,21 @@
+const people = ['Sakib', 'Tamim', 'Mashrafee']
+
+const a = 6;
+
+function test() {
+    console.log('Test');
+}
+
+// console.log(module)
+
+// module.exports = {
+//     people: people,
+//     a: a,
+//     test: test
+// };
+
+module.exports = {
+    people,
+    a,
+    test
+}
