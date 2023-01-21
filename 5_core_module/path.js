02b8c1ac8cfbeb15135ce297827d46ee1b45268a
hasanmehedi9034 <mehedi90344@gmail.com>
2023-01-21 11:55:12 +0600

    path module

---
+++ 5_core_module/path.js
@@ -0,0 +1,7 @@
+const path = require('path')
+
+const my_path = `F:/javascript/node.js_sumit/index.js`;
+
+// console.log(path.basename(my_path))
+// console.log(path.dirname(my_path));
+console.log(path.parse(my_path));
